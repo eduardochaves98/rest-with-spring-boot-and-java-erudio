9c0910dcedd94b25dae55d8662c6bf8bc261b8bb
eduardochaves98 <duduchaves98@gmail.com>
2022-10-11 16:35:54 -0300

    First commit

--- src/main/java/br/com/erudio/GreetingController.java
+++ src/main/java/br/com/erudio/GreetingController.java
@@ -1,6 +1,6 @@
 package br.com.erudio;
 
-import org.springframework.web.bind.annotation.GetMapping;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;

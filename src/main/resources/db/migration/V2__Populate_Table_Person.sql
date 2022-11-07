3ce9da7c573c24565ab421a8b6eb2041e57cce31
eduardochaves98 <duduchaves98@gmail.com>
2022-11-07 10:31:26 -0300

    Implemented migrations with flyway

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`) VALUES
+                                                                                (2,'Rua Roberto Resch, 20','Eduardo','Male','G Chaves'),
+                                                                                (6,'Rua Roberto Resch, 20','Eduardo','Male','Chaves'),
+                                                                                (7,'Rua Roberto Resch, 20','Eduardo','Male','Chaves');
